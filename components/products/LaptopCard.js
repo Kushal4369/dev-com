d7bc7612b04e6754ec8c9a1f12016da0c10456d6
Kushal4369 <143159808+Kushal4369@users.noreply.github.com>
2024-09-04 13:38:53 +0530

    Designed Laptops Page

--- components/products/LaptopCard.js
+++ components/products/LaptopCard.js
@@ -1,9 +1,27 @@
 import React from 'react'
-
+import Image from 'next/image'
+import Link from 'next/link'
 const LaptopCard = () => {
-  return (
-    <div>LaptopCard</div>
-  )
+    return (
+        <>
+            <div className="w-[18rem] sm:w-[20rem] h-[30rem] shadow-xl shadow-gray-800">
+                <Link href={''} className="block relative h-[16rem] rounded overflow-hidden">
+                    <Image alt="ecommerce" className="w-full h-full block" src="/laptop1.jpg" height={260} width={420} />
+                </Link>
+                <div className="text-center">
+                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY:<span className='text-gray-900 text-sm font-medium'>Laptops</span></h3>
+                    <h2 className=""></h2>
+                    <p className="mt-1">₹50000</p>
+                    <Link href={''} className='text-sm overflow-hidden'>HP Laptop 15s, 12th Gen Intel Core i3, 15.6-inch (39.6 cm), 8GB DDR4, 512GB SSD, Thin & Light, Dual Speakers (Win 11, MSO 2021, Silver, 1.69 kg), fq5007TU / FQ5327TU</Link>
+                    <div className='flex gap-2 text-sm m-2'>
+                        <button className="flex mx-auto  text-white bg-gray-800 border-0 py-2 px-4 focus:outline-none hover:bg-gray-700 rounded ">Add to Cart</button>
+                        <button className="flex mx-auto  text-white bg-gray-800 border-0 py-2 px-4 focus:outline-none hover:bg-gray-700 rounded ">Buy Now</button>
+
+                     </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default LaptopCard
--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,13 @@ export default function Home() {
   return (
     <div>
       <Image src={'/background.jpg'} alt="background" height={800} width={1000} className="z-0" />
-      
-    </div>
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-wrap w-full flex-col items-center text-center">
+          <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Explore modern gadgets now</h1>
+          <p className="lg:w-1/2 w-full leading-relaxed text-gray-500">Explore a wide range of tech products.</p>
+        </div>
+      </div>
+      </div>
 
-  );
+      );
 }
