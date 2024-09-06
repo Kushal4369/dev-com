cf153eda509c98fc1334d396b8ea9104e9c84b9d
Kushal4369 <143159808+Kushal4369@users.noreply.github.com>
2024-09-06 18:12:33 +0530

    Designed Bags Page

--- components/products/HeadphonesCard.js
+++ components/products/HeadphonesCard.js
@@ -5,27 +5,30 @@ import Image from 'next/image'
 const HeadphonesCard = () => {
     return (
         <>
-            <div className="w-[21rem] sm:w-[22rem] h-[30rem] shadow-xl shadow-gray-800">
-                <Link href={''}>
-                    <Image src={'/zebronics_headphones.jpg'} height={300} width={200} alt='Iphone'
-                        className='m-auto relative top-2'
-                    />
-                    <div className='flex flex-col relative top-1'>
-                        <p className='overflow-hidden px-4 h-16 text-sm'>ZEBRONICS Zeb-Thunder PRO On-Ear Wireless Headphone Up to 60 Hours Playback, Wired Mode, USB-C Type Charging(Black)</p>
-                        <h3 className="text-gray-500 text-xs text-center tracking-widest title-font mb-1 ">CATEGORY:<span className='text-gray-900 text-sm font-medium'>Headphones</span></h3>
-                        <p className="mt-1 text-center relative top-0">₹999</p>
+            <div className="w-[21rem] sm:w-[22rem] h-[31rem] shadow-xl shadow-gray-800 flex flex-col">
+                <div className='h-[340px] w-[350px] mx-auto'>
+                    <Link href={''} className='m-auto relative top-8'>
+                        <Image src={'/sony_headphones.jpg'} height={200} width={200} alt='headphones'
+                        className='mx-auto'/>
+                    </Link>
 
+                </div>
+                <div>
+                    <p className='overflow-hidden px-4 h-16 text-sm'>Redmi 13C (Stardust Black, 4GB RAM, 128GB Storage) | Powered by 4G Mediatek Helio G85 | 90Hz Display | 50MP AI Triple Camera</p>
+                    <h3 className="text-gray-500 text-xs text-center tracking-widest title-font mb-1">CATEGORY:<span className='text-gray-900 text-sm font-medium'>Smartphones</span></h3>
+                    <p className="text-center">₹50000</p>
+                </div>
 
-                        <div className='flex gap-2 text-sm m-2 items-center'>
-                            <button className="flex mx-auto  text-white bg-gray-800 border-0 py-2 px-4 focus:outline-none hover:bg-gray-700 rounded ">Add to Cart</button>
-                            <button className="flex mx-auto  text-white bg-gray-800 border-0 py-2 px-4 focus:outline-none hover:bg-gray-700 rounded ">Buy Now</button>
+                <div className='flex gap-2 text-sm  items-center'>
+                    <button className="flex mx-auto  text-white bg-gray-800 border-0 py-2 px-4 focus:outline-none hover:bg-gray-700 rounded ">Add to Cart</button>
+                    <button className="flex mx-auto  text-white bg-gray-800 border-0 py-2 px-4 focus:outline-none hover:bg-gray-700 rounded ">Buy Now</button>
 
-                        </div>
-                    </div>
-
-                </Link>
-
+                </div>
             </div>
+
+
+
+
         </>
     )
 }
